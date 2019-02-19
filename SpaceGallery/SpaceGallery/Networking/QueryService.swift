6e0b9328289999d69a57709da5deb2ecba252220
Leonardo Domingues <leodmgs@gmail.com>
2019-02-19 18:24:24 -0300

    Add service to request data from NASA API with a search string

--- SpaceGallery/SpaceGallery/Networking/QueryService.swift
+++ SpaceGallery/SpaceGallery/Networking/QueryService.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import SwiftyJSON
+import TRON
+
+class QueryService {
+    
+    private let urlBaseString: String = "https://images-api.nasa.gov"
+    
+    func searchImages(
+        query: String,
+        _ limitResults: Int? = 50,
+        completion: @escaping (Array<ImageMetadata>) -> ()) {
+        
+        let urlObject =
+            URL(string: "\(urlBaseString)/search?q=\(query)&media_type=image")!
+        let task = URLSession.shared.dataTask(with: urlObject) {
+            data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let httpResp = response as? HTTPURLResponse,
+                (200...299).contains(httpResp.statusCode) else {
+                    print(response.debugDescription)
+                    return
+            }
+            if let data = data {
+                do {
+                    let json = try JSON(data: data)
+                    let imagesMetadata =
+                        self.items(jsonItems: json["collection"]["items"])
+                    completion(imagesMetadata)
+                } catch SwiftyJSONError.invalidJSON {
+                    print("[Error] Invalid JSON.")
+                } catch {
+                    print("Unexpected error: \(error)")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    private func items(jsonItems: JSON) -> Array<ImageMetadata> {
+        var items = Array<ImageMetadata>()
+        for item in jsonItems.arrayValue {
+            items.append(ImageMetadata(json: item))
+        }
+        return items
+    }
+    
+}
