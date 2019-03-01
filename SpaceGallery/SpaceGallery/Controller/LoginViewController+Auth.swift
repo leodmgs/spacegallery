06275f4ad60b1e8514d6386b33eb92da7f79eb48
Leonardo Domingues <leodmgs@gmail.com>
2019-03-01 20:07:35 -0300

    Add extension to provide UIColor customizations

---
+++ SpaceGallery/SpaceGallery/Controller/LoginViewController+Auth.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController+Auth.swift
+//  SpaceGallery
+//
+//  Created by Leonardo Domingues on 3/1/19.
+//  Copyright © 2019 Leonardo Domingues. All rights reserved.
+//
+
+import Foundation
