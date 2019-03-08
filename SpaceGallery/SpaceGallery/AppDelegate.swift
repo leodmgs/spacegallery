1feee122e6fdca2fda5107dee4d5215bf5fde84e
Leonardo Domingues <leodmgs@gmail.com>
2019-03-07 22:01:49 -0300

    Start app with login page as the main root controller

--- SpaceGallery/SpaceGallery/AppDelegate.swift
+++ SpaceGallery/SpaceGallery/AppDelegate.swift
@@ -19,11 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        let navigationController = UINavigationController()
         let loginViewController = LoginViewController()
-        let searchViewController = SearchViewController()
-        navigationController.viewControllers = [
-            loginViewController, searchViewController]
         window?.rootViewController = loginViewController
         window?.makeKeyAndVisible()
         
