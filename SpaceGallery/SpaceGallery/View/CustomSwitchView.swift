9ccd11f19fd149507987670381363c8c19ffc0b2
Leonardo Domingues <leodmgs@gmail.com>
2019-03-07 21:52:58 -0300

    Customize a switch view to allow to add a label description for that

--- SpaceGallery/SpaceGallery/View/CustomSwitchView.swift
+++ SpaceGallery/SpaceGallery/View/CustomSwitchView.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomSwitchView: UIView {
+    
+    let option: UISwitch = {
+        let switchView = UISwitch()
+        switchView.translatesAutoresizingMaskIntoConstraints = false
+        switchView.isOn = false
+        return switchView
+    }()
+    
+    private let switchText: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var text: String? {
+        didSet {
+            guard let text = text else { return }
+            DispatchQueue.main.async {
+                self.switchText.attributedText = self.attributedText(text: text)
+            }
+        }
+    }
+    
+    private let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alignment = .center
+        stack.spacing = 10
+        return stack
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented")
+    }
+    
+    private func setupView() {
+        stackView.addArrangedSubview(option)
+        stackView.addArrangedSubview(switchText)
+        addSubview(stackView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            option.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            switchText.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            ])
+    }
+    
+    private func attributedText(text: String) -> NSAttributedString {
+        let attributedText = NSAttributedString(
+            string: text,
+            attributes: [
+                NSAttributedString.Key.font : UIFont.systemFont(
+                    ofSize: 14,
+                    weight: .regular),
+                NSAttributedString.Key.foregroundColor : SGColor.sgWhiteColor])
+        return attributedText
+    }
+    
+}
